82589c0854c537cc6fc42201cf93805969d0127d
aaron_cargame <contacto@cargame.mx>
2022-01-18 12:17:55 -0700

    form en modal

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
-var phoneBtn = document.getElementById('phone-btn');
+var phoneBtn = document.getElementById("phone-btn");
 var ancho = window.innerWidth;
 
 phoneBtn.onclick = () => {
-    if (ancho <= 600) {
-        alert("lanza app de telefono");
-    } else {
-        alert("whatsapp web");
-    }
-}
+  if (ancho <= 600) {
+    alert("lanza app de telefono");
+  } else {
+    alert("whatsapp web");
+  }
+};
+
+// modal 
+var modal = document.getElementById("modalForm");
+
+var btn = document.getElementsByClassName("contact-btn")[0];
+var btn2 = document.getElementsByClassName("contact-btn")[1];
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+btn2.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
