9656ca226d89970fda15938365c99de7e27d403f
aaron_cargame <contacto@cargame.mx>
2022-02-04 13:32:00 -0700

    add color to btn hover and animate the services hover

--- main.js
+++ main.js
@@ -11,70 +11,6 @@ phoneBtn.onclick = () => {
   }
 };
 
-var info1 = document.getElementById("info1");
-var info2 = document.getElementById("info2");
-var info3 = document.getElementById("info3");
-
-var moreServiceInfo = document.getElementsByClassName("moreInfo")[0];
-var serviceInfoContainer = document.getElementsByClassName("service-info")[0];
-moreServiceInfo.onclick = () => {
-  if (open == false) {
-    open = true;
-    serviceInfoContainer.style.display = "flex";
-    serviceInfoContainer.style.flexDirection = "column";
-    serviceInfoContainer.style.alignItems = "stretch";
-    serviceInfoContainer.style.height = "auto";
-    info1.style.display = "block";
-  } else {
-    open = false;
-    serviceInfoContainer.style.display = "flex";
-    serviceInfoContainer.style.flexDirection = "row";
-    serviceInfoContainer.style.alignItems = "center";
-    serviceInfoContainer.style.height = "unset";
-    info1.style.display = "none";
-  }
-};
-
-var moreServiceInfo1 = document.getElementsByClassName("moreInfo")[1];
-var serviceInfoContainer1 = document.getElementsByClassName("service-info")[1];
-moreServiceInfo1.onclick = () => {
-  if (open == false) {
-    open = true;
-    serviceInfoContainer1.style.display = "flex";
-    serviceInfoContainer1.style.flexDirection = "column";
-    serviceInfoContainer1.style.alignItems = "stretch";
-    serviceInfoContainer1.style.height = "auto";
-    info2.style.display = "block";
-  } else {
-    open = false;
-    serviceInfoContainer1.style.display = "flex";
-    serviceInfoContainer1.style.flexDirection = "row";
-    serviceInfoContainer1.style.alignItems = "center";
-    serviceInfoContainer1.style.height = "unset";
-    info2.style.display = "none";
-  }
-};
-
-var moreServiceInfo2 = document.getElementsByClassName("moreInfo")[2];
-var serviceInfoContainer2 = document.getElementsByClassName("service-info")[2];
-moreServiceInfo2.onclick = () => {
-  if (open == false) {
-    open = true;
-    serviceInfoContainer2.style.display = "flex";
-    serviceInfoContainer2.style.flexDirection = "column";
-    serviceInfoContainer2.style.alignItems = "stretch";
-    serviceInfoContainer2.style.height = "auto";
-    info3.style.display = "block";
-  } else {
-    open = false;
-    serviceInfoContainer2.style.display = "flex";
-    serviceInfoContainer2.style.flexDirection = "row";
-    serviceInfoContainer2.style.alignItems = "center";
-    serviceInfoContainer2.style.height = "unset";
-    info3.style.display = "none";
-  }
-}
-
 // modal
 var modal = document.getElementById("modalForm");
 
@@ -101,5 +37,5 @@ window.onclick = function (event) {
   }
 };
 
-var winfo = document.getElementById("winfo");
-winfo.innerText = window.innerWidth;
+// var winfo = document.getElementById("winfo");
+// winfo.innerText = window.innerWidth;
