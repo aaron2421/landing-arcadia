13af06f8c326404772014b16f7b1e4e0355b0b1d
aaron_cargame <contacto@cargame.mx>
2022-02-18 13:27:21 -0700

    remove the firebase part, and the form

--- main.js
+++ main.js
@@ -1,51 +1,19 @@
 var phoneBtn = document.getElementById("phone-btn");
-var ancho = window.innerWidth;
-var open = false;
-// var contactBtn = document.getElementsByClassName("contact-btn")[0];
-// var contactBtn2 = document.getElementsByClassName("contact-btn")[1];
+var contactBtn = document.getElementsByClassName("contact-btn")[0];
+var contactBtn2 = document.getElementsByClassName("contact-btn")[1];
 
 phoneBtn.onclick = () => {
   if (ancho <= 600) {
     location.href = "tel:+523320350386";
   } else {
     location.href = "https://api.whatsapp.com/send?phone=+523320350386";
-    // window.open("https://api.whatsapp.com/send?phone=+523320350386");
   }
 };
 
-// contactBtn.onclick = () => {
-//   location.href = "https://calendly.com/arcadia-almacenadoras/30min";
-// }
+contactBtn.onclick = () => {
+  location.href = "https://calendly.com/arcadia-almacenadoras/30min";
+}
 
-// contactBtn2.onclick = () => {
-//   location.href = "https://calendly.com/arcadia-almacenadoras/30min";
-// }
-
-// modal
-var modal = document.getElementById("modalForm");
-
-var btn = document.getElementsByClassName("contact-btn")[0];
-var btn2 = document.getElementsByClassName("contact-btn")[1];
-
-var span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function () {
-  modal.style.display = "block";
-};
-
-btn2.onclick = function () {
-  modal.style.display = "block";
-};
-
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
-
-// var winfo = document.getElementById("winfo");
-// winfo.innerText = window.innerWidth;
+contactBtn2.onclick = () => {
+  location.href = "https://calendly.com/arcadia-almacenadoras/30min";
+}
