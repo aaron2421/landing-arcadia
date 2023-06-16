0695c7beeae3507376c560790a81bbc02dc9347a
Daniel Gonzalez Placito <daplacito@outlook.com>
2023-06-16 11:20:46 -0700

    Actualizacion de numero telefonico y funcionalidad de llamar en navegadores de dispositivos moviles

--- main.js
+++ main.js
@@ -3,17 +3,25 @@ window.onload = () => {
   var contactBtns = document.getElementsByClassName("contact-btn");
   var ancho = window.innerWidth;
 
-  phoneBtn.onclick = () => {
-    if (ancho <= 600) {
-      location.href = "tel:+523343579334";
-    } else {
-      location.href = "https://api.whatsapp.com/send?phone=+523343579334";
-    }
-  };
+  // phoneBtn.onclick = () => {
+  //   if (ancho <= 600) {
+  //     location.href = "tel:+523343530703";
+  //   } else {
+  //     var numeroTelefono = "+523343530703"; // Reemplaza con el número de teléfono al que deseas enviar el mensaje
+  //     var mensaje = "¡Hola!"; // Reemplaza con el mensaje que deseas enviar
 
-  for(var i = 0; i<contactBtns.length; i++) {
+  //     var url =
+  //       "https://wa.me/" +
+  //       numeroTelefono +
+  //       "?text=" +
+  //       encodeURIComponent(mensaje);
+  //     window.open(url);
+  //   }
+  // };
+
+  for (var i = 0; i < contactBtns.length; i++) {
     contactBtns[i].onclick = () => {
       location.href = "https://calendly.com/arcadia-almacenadoras/30min";
-    }
+    };
   }
-}
+};
